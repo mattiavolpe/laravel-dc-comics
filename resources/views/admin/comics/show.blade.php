9826ab186d06c92e0a633283410175c46e7a5f04
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-05-29 18:01:11 +0200

    ADD single comic admin page

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -2,9 +2,25 @@
 
 @section("content")
 <div class="container py-5">
-  <div class="card">
+  <div class="card w-50 mx-auto">
     <div class="card-header">
-      <h2></h2>
+      <img src="{{ $comic -> thumb }}" alt="" class="card-img-top">
+      <h2><--{{ $comic -> id }}--&gt;: {{ $comic -> title }}</h2>
+      <p>{{ $comic -> description }}</p>
+      <h6>Price:
+        <span class="fw-normal">{{ $comic -> price }}</span>
+      </h6>
+      
+      <h6>Series:
+        <span class="fw-normal">{{ $comic -> series }}</span>
+      </h6>
+      
+      <h6>Sale date:
+        <span class="fw-normal">{{ $comic -> sale_date }}</span>
+      </h6>
+      <h6>Type:
+        <span class="fw-normal text-capitalize">{{ $comic -> type }}</span>
+      </h6>
     </div>
   </div>
 </div>
