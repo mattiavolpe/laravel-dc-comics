1ad7525309c0980359453faa972b15604af1a94e
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-01 14:06:38 +0200

    ADD missing $ character on price outputs

--- resources/views/partials/comic_details_bottom.blade.php
+++ resources/views/partials/comic_details_bottom.blade.php
@@ -9,11 +9,6 @@
         </div>
         <hr class="border-secondary">
         <div class="d-flex">
-          <span class="w-25 flex-shrink-0">U.S. Price:</span>
-          <span>{{ $data['comic'][0]->price }}</span>
-        </div>
-        <hr class="border-secondary">
-        <div class="d-flex">
           <span class="w-25 flex-shrink-0">On Sale Date:</span>
           <span>{{ date_format(date_create($data['comic'][0]->sale_date), "M d Y") }}</span>
         </div>
--- resources/views/partials/comic_details_top.blade.php
+++ resources/views/partials/comic_details_top.blade.php
@@ -3,7 +3,7 @@
     <h3 class="text-uppercase mb-0">{{ $data['comic'][0]->title }}</h3>
     <div id="price_availability" class="d-flex justify-content-between align-items-center text-white fw-bold my-4">
       <div class="mx-4 py-3">
-        <span class="text-success">U.S. Price:</span> {{ $data['comic'][0]->price }}
+        <span class="text-success">U.S. Price:</span> ${{ $data['comic'][0]->price }}
       </div>
     </div>
     <p class="mb-0">{{ $data['comic'][0]->description }}</p>
